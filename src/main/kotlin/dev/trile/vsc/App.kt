3ca5f7de09d360e4ab7c777d9377147b03cd59fa
Tri Le <thanhtrixx@gmail.com>
2021-08-14 10:16:11 +0700

    Add dependencies

--- src/main/kotlin/dev/trile/vsc/App.kt
+++ src/main/kotlin/dev/trile/vsc/App.kt
@@ -3,13 +3,20 @@
  */
 package dev.trile.vsc
 
-class App {
-    val greeting: String
-        get() {
-            return "Hello World!"
-        }
-}
+import io.ktor.application.*
+import io.ktor.response.*
+import io.ktor.routing.*
+import io.ktor.server.engine.*
+import io.ktor.server.netty.*
 
 fun main() {
-    println(App().greeting)
+
+  embeddedServer(Netty, port = 8000) {
+    routing {
+      get("/") {
+        call.respondText("Hello, world!")
+      }
+    }
+  }.start(wait = true)
+
 }
